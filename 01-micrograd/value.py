667ea42da9fb0f9f28fca22c29f85f91b8c8c11c
Nandani Patil <nandanipatil68@gmail.com>
2026-08-27 08:22:00 +0530

    Add micrograd Value class multiplication logic

---
+++ 01-micrograd/value.py
@@ -0,0 +1,16 @@
+class Value:
+    def __init__(self, data):
+        self.data = data
+
+    def __repr__(self):
+        return f"Value(data={self.data})"
+
+    def __add__(self, other):
+        out = Value(self.data + other.data)
+        return out
+
+
+a = Value(2.0)
+b = Value(-3.0)
+
+print(a + b)
